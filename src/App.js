fb0903c04d624e38c3874eb4bb11731680b42cec
Thomas Bockhorn <thomasbockhorn1863@gmail.com>
2018-09-15 02:25:56 -0500

    added reads area

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import React, {
 } from 'react';
 import './App.css';
 import Navbar from './components/navbar';
+import Readsdisplay from './components/readsdisplay';
 
 class App extends Component {
   render() {
     return ( <
+      React.Fragment >
+      <
       Navbar / >
+      <
+      Readsdisplay / >
+      <
+      /React.Fragment>
     );
   }
 }
---
+++ src/components/readsdisplay.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class readsdisplay extends Component {
+    state = {}
+    render() {
+        return (
+            <div className="jumbotron" id="read">
+            </div>
+        );
+    }
+}
+
+export default readsdisplay;
