fd45534cc911ece41b3f1c5a138eb9f78d01ad6e
Thomas Bockhorn <thomasbockhorn1863@gmail.com>
2018-10-01 12:10:00 -0500

    Search text and button works

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 
 class navbar extends Component {
-    state = {}
+    state = { input: "" }
+
+    //This will get the value of the search term and input it into the state
+    handleChange = (e) => {
+        this.setState({ input: e.target.value });
+    };
+
+    //This button will get the value and conduct the search
+    handleButton = () => {
+        alert(this.state.input);
+    };
+
     render() {
         return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark" id="nav">
@@ -11,8 +22,8 @@ class navbar extends Component {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <form className="form-inline my-2 my-lg-0">
-                        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" onChange={this.handleChange}></input>
+                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit" onClick={this.handleButton}>Search</button>
                     </form>
                 </div>
             </nav>
